b2a9c40f8fc264e5ae75bcd5228100c5ae28df8a
Sudhakar <sidhu1171bansal@gmail.com>
2023-08-03 15:03:58 +0530

    Glowing Bulb

---
+++ Day-02(exrecise)/script.js
@@ -0,0 +1,10 @@
+function chngeColor() {
+    if (document.getElementsByClassName('bulb')[0].style.backgroundColor == "yellow") {
+        document.getElementsByClassName('bulb')[0].style.cssText = "border: 1px solid grey;"
+        document.getElementsByTagName('button')[0].innerHTML="Switch On"
+    } 
+    else {
+        document.getElementsByClassName('bulb')[0].style.cssText = "background-color : yellow; border: none;  box-shadow: yellow 0px 0px 276px 153px;"
+        document.getElementsByTagName('button')[0].innerHTML="Switch Off"
+    }
+}
